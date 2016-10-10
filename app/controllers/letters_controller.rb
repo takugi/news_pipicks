b9c62e264f58d67bd0a3c2663b92db88c48281a8
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-10 16:51:26 +0900

    make new and create action

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,2 +1,25 @@
 class LettersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @letter = Letter.new
+  end
+
+  def create
+    letter = Letter.new(url: url_params)
+    if letter.save
+      letter.create_letter
+      redirect_to root_path
+    else
+      redirect_to new_letter_path
+    end
+  end
+
+
+  private
+  def url_params
+    params.require(:url)
+  end
 end
