354a1cb3dec8500fe4eed2aead5b66cc1d333187
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-10 16:53:26 +0900

    make letters table site_name and description, url columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008022329) do
+ActiveRecord::Schema.define(version: 20161010060121) do
 
   create_table "letters", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.string   "image",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "title",       limit: 255
+    t.string   "image",       limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "site_name",   limit: 255
+    t.string   "description", limit: 255
+    t.string   "url",         limit: 255
   end
 
 end
