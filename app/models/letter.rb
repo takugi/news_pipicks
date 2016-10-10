7262e5d61471dcd2ad551d5c03823a1c16c7c5c9
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-10 16:52:03 +0900

    make validation :url and create_letter method

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,2 +1,27 @@
 class Letter < ActiveRecord::Base
+
+  validates :url, format: URI::regexp(%w(http https))
+
+  def create_letter
+    agent = Mechanize.new
+    page = agent.get(self.url)
+    elements = page.search('meta')
+
+    letter = {}
+
+    elements.each do |ele|
+      property = ele.get_attribute(:property)
+      if property == "og:title"
+        letter[:title] = ele.get_attribute(:content)
+      elsif property == "og:image"
+        letter[:image] = ele.get_attribute(:content)
+      elsif property == "og:site_name"
+        letter[:site_name] = ele.get_attribute(:content)
+      elsif property == "og:description"
+        letter[:description] = ele.get_attribute(:content)
+      end
+    end
+
+    self.update(title: letter[:title], image: letter[:image], site_name: letter[:site_name], description: letter[:description])
+  end
 end
