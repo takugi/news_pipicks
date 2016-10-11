484bf37547fa7347c756ac01b436c5686a5a2e87
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-11 17:13:17 +0900

    add edit and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user == @user
+      if @user.update(update_params)
+        redirect_to root_path
+      else
+        redirect_to edit_user_path(current_user)
+      end
+    else
+      redirect_to root_path
+    end
   end
 
   private
@@ -10,4 +26,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update_params
+    params.require(:user).permit(:avatar, :last_name, :firstname, :profile)
+  end
 end
