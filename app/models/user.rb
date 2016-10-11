31869f09ff73dfe029d5f4139929a1e36ec6f16b
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-11 13:46:41 +0900

    make full_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+    "#{last_name} #{first_name}"
+  end
+
 end
