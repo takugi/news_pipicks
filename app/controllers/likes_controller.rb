a33db749cba4214e77453cb0f08e5af1e743c29f
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-14 18:01:10 +0900

    add create and destroy action, define set_letter and likes_params

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,30 @@
 class LikesController < ApplicationController
+
+  before_action :set_letter, only: [:create, :destroy]
+
+  def create
+    @like = Like.new(likes_params)
+    if @like.save
+      redirect_to letter_path(@letter)
+    else
+      redirect_to letter_path(@letter)
+    end
+  end
+
+  def destroy
+    if Like.find_by(likes_params).destroy
+      redirect_to letter_path(@letter)
+    else
+      redirect_to letter_path(@letter)
+    end
+  end
+
+  private
+  def likes_params
+    params.permit(:comment_id).merge(user_id: current_user.id)
+  end
+
+  def set_letter
+    @letter = Letter.find(params[:letter_id])
+  end
 end
