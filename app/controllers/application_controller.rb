930da8cc28547de6dc2e3f878b439ffab79ba230
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-17 17:14:25 +0900

    correct from firstname to first_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :last_name, :firstname, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :last_name, :first_name, :profile])
   end
 end
