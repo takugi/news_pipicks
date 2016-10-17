5a64033391a63ec45562034be7f4e820863549e5
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-17 14:53:24 +0900

    add some configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,12 +7,16 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'devise'
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include LoginMacros, type: :controller
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
+  config.include FactoryGirl::Syntax::Methods
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
