89a88a31231cae1379832fac723fdc176bdcc5f1
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-20 15:50:34 +0900

    add store_dir method

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,4 +6,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
 end
