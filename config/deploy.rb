c180ed495c19bb58becc3528b6386a07f3a02ef4
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-20 11:53:17 +0900

    make unicorn and deploy setting file

---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'news_pipicks'
+
+set :repo_url, 'git@github.com:https://github.com/takugi/news_pipicks.git'
+
+set :branch, 'master'
+
+set :deploy_to, '/var/www/news_pipicks'
+
+set :linked_files, fetch(:linked_files, []).push('config/setting.yml')
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+set :keep_releases, 5
+
+set :rbenv_ruby, '2.3.1'
+
+set :log_level, :debug
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+end
