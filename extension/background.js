d6ece83ec7e11669d02419f551e21b0e3de8ddfc
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-22 17:58:34 +0900

    correct

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,7 @@
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.getSelected(window.id, function (tab) {
     var url = tab.url;
-    chrome.tabs.create({'url': "http://localhost:3000/letters/new"});
+    chrome.tabs.create({'url': "http://52.193.198.83/letters/new"});
     chrome.tabs.executeScript(null,{
       "code": "document.getElementsByClassName('container__pick-form__url-group__url-value')[0].value='"+url+"'"
     });
