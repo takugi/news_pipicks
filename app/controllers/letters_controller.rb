247c65ac305ee74e5e501f13e411a955d39e1c6a
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-25 21:44:30 +0900

    make before action set_letters, and change not to post same letter

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,9 +1,9 @@
 class LettersController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_letters, only: [:index, :create]
 
   def index
-    @letters = Letter.order("created_at desc")
     @better_letters = Letter.with_better_letters
   end
 
@@ -11,14 +11,19 @@ class LettersController < ApplicationController
   end
 
   def create
-    letter = Letter.new(url_params)
-    if letter.save
-      letter.create_letter
-      flash[:notice] = "正常に投稿できました。"
-      redirect_to letter_path(letter)
+    unless @letters.pluck(:url).include?(url_params[:url])
+      letter = Letter.new(url_params)
+      if letter.save
+        letter.create_letter
+        flash[:notice] = "正常に投稿できました。"
+        redirect_to letter_path(letter)
+      else
+        flash[:alert] = "投稿に失敗しました。"
+        redirect_to :back
+      end
     else
-      flash[:alert] = "投稿に失敗しました。"
-      redirect_to :back
+      @select_letter = Letter.find_by(url: url_params[:url])
+      redirect_to letter_path(@select_letter)
     end
   end
 
@@ -29,6 +34,10 @@ class LettersController < ApplicationController
   end
 
   private
+    def set_letters
+      @letters = Letter.order("created_at desc")
+    end
+
     def url_params
       params.permit(:url)
     end
