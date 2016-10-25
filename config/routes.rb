049f98079727ad37873450d33ff34fbea65445c1
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-25 19:59:51 +0900

    remove routing letters new

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-  resources :letters, only: [:index, :show, :new, :create] do
+  resources :letters, only: [:index, :show, :create] do
     resources :comments, only: [:create, :destroy] do
       resources :likes, only: [:create, :destroy]
     end
