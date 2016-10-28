d8216a86fb9ff62efe9271e53497bbf838799faf
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-10-28 18:50:47 +0900

    remove set_letters method, add @letters to index and create action

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,16 +1,18 @@
 class LettersController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_letters, only: [:index, :create]
 
   def index
+    @letters = Letter.order("created_at desc").first(9)
     @better_letters = Letter.with_better_letters
+    @categories = Letter.select_categories
   end
 
   def new
   end
 
   def create
+    @letters = Letter.order("created_at desc")
     unless @letters.pluck(:url).include?(url_params[:url])
       letter = Letter.new(url_params)
       if letter.save
@@ -34,10 +36,6 @@ class LettersController < ApplicationController
   end
 
   private
-    def set_letters
-      @letters = Letter.order("created_at desc")
-    end
-
     def url_params
       params.permit(:url)
     end
