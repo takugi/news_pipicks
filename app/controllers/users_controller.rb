5a70ec86a6557f626d5bb79ca093ccba1d69518a
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-11-01 20:55:12 +0900

    add index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_user
+  before_action :set_user, except: :index
+
+  def index
+    @users = User.all
+  end
 
   def show
     @letters = @user.user_letters
