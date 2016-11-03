d26fa1fcb7e77c3546fd6d8357caa228be9e197f
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-11-03 20:49:40 +0900

    change data type of url column from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101060112) do
+ActiveRecord::Schema.define(version: 20161103114014) do
 
   create_table "books", force: :cascade do |t|
     t.integer  "user_id",      limit: 4
-    t.string   "title",        limit: 255,              null: false
-    t.string   "image",        limit: 255, default: "", null: false
-    t.string   "url",          limit: 255, default: "", null: false
-    t.string   "author",       limit: 255, default: "", null: false
-    t.string   "manufacturer", limit: 255, default: "", null: false
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.string   "title",        limit: 255,                null: false
+    t.string   "image",        limit: 255,   default: "", null: false
+    t.string   "author",       limit: 255,   default: "", null: false
+    t.string   "manufacturer", limit: 255,   default: "", null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.text     "url",          limit: 65535
   end
 
   add_index "books", ["user_id"], name: "index_books_on_user_id", using: :btree
