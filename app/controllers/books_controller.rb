0c5a916fbb46142c6b4d800b55aa80816c532367
長野拓義 <naganotakugi@naganotakugi-no-MacBook-Pro.local>
2016-11-04 14:05:08 +0900

    change writing position from controller to model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,37 +7,18 @@ class BooksController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
   def new
-    if params[:keyword].present?
-      Amazon::Ecs.debug = true
-
-      books = Amazon::Ecs.item_search(
-        params[:keyword],
-        search_index: 'Books',
-        dataType: 'script',
-        response_group: 'ItemAttributes, Images',
-        country: 'jp',
-        power: 'Not kindle'
-      )
-
-      @search_books = []
-      books.items.each do |item|
-        book = Book.new(
-          title: item.get('ItemAttributes/Title'),
-          image: item.get('LargeImage/URL'),
-          url: item.get('DetailPageURL'),
-          author: item.get('ItemAttributes/Author'),
-          manufacturer: item.get('ItemAttributes/Manufacturer'),
-        )
-        @search_books.push(book)
-      end
-    end
+    @search_books = Book.search_book(params[:keyword])
   end
 
   def create
     book = Book.new(create_params)
     if book.save
-      flash[:notice] = "おすすめ本を投稿しました。"
+      flash[:notice] = ""
       redirect_to user_book_path(current_user, book)
     else
       flash[:alert] = "おすすめ本の投稿に失敗しました。"
